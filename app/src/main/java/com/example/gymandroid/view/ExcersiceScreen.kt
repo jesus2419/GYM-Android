8aadf4e55e75daef8050166130c687adc72f42c9
unknown <jesus.osorio255@gmail.com>
2025-05-02 11:55:55 -0600

    pantalla de ejercicios arreglada

--- app/src/main/java/com/example/gymandroid/view/ExcersiceScreen.kt
+++ app/src/main/java/com/example/gymandroid/view/ExcersiceScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -35,12 +36,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -61,10 +64,15 @@ fun ExerciseDetailScreen(
     navController: NavController,
     viewModel: HomeViewModel = viewModel()
 ) {
-    // Buscar el ejercicio en las categorías dummy
-    val exercise = remember(exerciseId) {
-        dummyCategories.flatMap { it.exercises }.firstOrNull { it.id == exerciseId }
-    }
+
+    val categories by viewModel.categories.collectAsState()
+
+
+
+
+    val exercise = categories
+        .flatMap { it.exercises }
+        .firstOrNull { it.id == exerciseId }
 
     // Si no se encuentra el ejercicio, mostrar error
     if (exercise == null) {
